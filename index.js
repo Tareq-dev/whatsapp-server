c1c959e988f5f12659cbedc8223e6d5afcbe6c9a
Tareq-dev <tareque.dev@gmail.com>
2023-05-12 15:04:49 +0600

    clinet  remove

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 // const Ably = require("ably");
 
-const { Client, LocalAuth } = require("whatsapp-web.js");
+// const { Client, LocalAuth } = require("whatsapp-web.js");
 const fileUpload = require("express-fileupload");
 
 app.use(cors());
@@ -15,7 +15,7 @@ app.use(fileUpload());
 
 const port = process.env.PORT || 5000;
 
-const client = new Client({ authStrategy: new LocalAuth() });
+// const client = new Client({ authStrategy: new LocalAuth() });
 
 // const ably = new Ably.Realtime(
 //   "VSU5GA.7M4Y5Q:4Tok9TlkaNq5T8u5dKnJ42pu3oZrH0GYqKpkNPVqsHE"
@@ -34,12 +34,12 @@ app.get("/scan", (req, res) => {
   // });
 });
 
-client.on("ready", () => {
-  console.log("client is ready");
-  // channel1.publish("client-ready", "Client is ready!");
-});
+// client.on("ready", () => {
+//   console.log("client is ready");
+//   // channel1.publish("client-ready", "Client is ready!");
+// });
 
-client.initialize();
+// client.initialize();
 
 app.get("/", (req, res) => {
   res.send("Hello World of ph");
