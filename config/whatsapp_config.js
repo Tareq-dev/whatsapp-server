401e205e4e4eaa34b5e2266115a207f7eb73e495
Tareq-dev <tareque.dev@gmail.com>
2023-05-19 14:14:46 +0600

    remove session

--- config/whatsapp_config.js
+++ config/whatsapp_config.js
@@ -1,4 +1,5 @@
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+
 const client = new Client({ authStrategy: new LocalAuth() });
 
 module.exports = { client, MessageMedia, LocalAuth };
