faa1bc3a58b3bc131dba8811d57288419a23537d
Tareq-dev <tareque.dev@gmail.com>
2023-05-19 19:04:21 +0600

    local

--- config/db.js
+++ config/db.js
@@ -2,19 +2,19 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const db = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "whatsbulk",
-});
 // const db = mysql.createPool({
-//   host: process.env.host,
-//   user: process.env.user,
-//   password: process.env.password,
-//   database: process.env.database,
-//   port: 3306,
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "",
+//   database: "whatsbulk",
 // });
+const db = mysql.createPool({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  port: 3306,
+});
 
 db.getConnection(function (err, conn) {
   if (err) {
