c1560bbe54040e9bc2424e0e208b519cd50fe8a3
Tareq-dev <tareque.dev@gmail.com>
2023-06-11 21:38:29 +0600

    added new connection system

--- config/db.js
+++ config/db.js
@@ -16,13 +16,35 @@ const db = mysql.createPool({
   port: 3306,
 });
 
-db.getConnection(function (err, conn) {
+// db.getConnection(function (err, conn) {
+//   conn.release();
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("Successfully connect to the database.");
+//   // db.releaseConnection(conn);
+//   console.log(conn);
+// });
+
+db.getConnection(function (err, connection) {
   if (err) {
     console.log(err);
-  }
-  console.log("Successfully connect to the database.");
-  conn.release();
-  // db.releaseConnection(conn);
+  } // not connected!
+
+  // Use the connection
+  connection.query(
+    "SELECT email FROM register",
+    function (error, results, fields) {
+      // When done with the connection, release it.
+      console.log("Successfully connect to the database.");
+      connection.release();
+
+      // Handle error after the release.
+      if (error) throw error;
+
+      // Don't use the connection here, it has been returned to the pool.
+    }
+  );
 });
 
 module.exports = db;
